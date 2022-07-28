c26c9e72172c7fabc8cbeee07cd4a2d1bbcf4345
VitorSantosH <wytorh@gmail.com>
2022-07-27 23:05:59 -0300

    lojas ...

--- src/components/home/homeOwnerMaster/lojas/LojasDetalhe.jsx
+++ src/components/home/homeOwnerMaster/lojas/LojasDetalhe.jsx
@@ -7,21 +7,34 @@ import './LojasDetalhe.css'
 //imgs 
 import setasImg from '../../../../assets/Setas.png';
 import gitCarregamento from '../../../../assets/Rolling.gif';
+import btnVoltar from '../../../../assets/bntVoltar.png';
+import btnIr from '../../../../assets/arrow-circle-right.png';
+
 
 const LojasDetalhe = (props) => {
 
     const [stateStoreDetalhe, setStateStoreDetalhe] = useState({
         isLoading: false,
-        store: undefined
+        store: undefined,
+        lengthCarrosel: 0,
+        carroselPosition: 0,
+        imgs: []
     })
 
     useEffect(() => {
+
+        if (stateStoreDetalhe.store != undefined && stateStoreDetalhe.lengthCarrosel == 0) {
+            setStateStoreDetalhe({
+                ...stateStoreDetalhe,
+                lengthCarrosel: stateStoreDetalhe.store.fimecoData.fimistes.length ? stateStoreDetalhe.store.fimecoData.fimistes.length : 0
+            })
+        }
 
         if (stateStoreDetalhe.isLoading === false) {
             getStore(props.codStore)
         }
 
-    }, [stateStoreDetalhe.isLoading])
+    }, [stateStoreDetalhe.isLoading, stateStoreDetalhe.store])
 
     async function getStore(codStore) {
 
@@ -48,6 +61,30 @@ const LojasDetalhe = (props) => {
         })
 
         return spans
+    }
+
+    function GenerateCarrosel() {
+
+        if (stateStoreDetalhe.store === undefined) return <></>
+
+        const imgs = stateStoreDetalhe.store.fimecoData.fimistes.map((img, i) => {
+
+            return <img
+                key={`${i}-${stateStoreDetalhe.carroselPosition}`}
+                
+                src={img.fileImgUriExternal}
+                numberimage={i}
+                alt=""
+                className={i == stateStoreDetalhe.carroselPosition ? 'selected' : 'notSelected'}
+            />
+
+        })
+
+        return <>
+            {imgs}
+        </>
+
+
     }
 
 
@@ -113,8 +150,48 @@ const LojasDetalhe = (props) => {
                             <div className="imgPrincipal">
                                 <img src={stateStoreDetalhe.store.fimecoData.fimistCover.fileImgUriExternal} alt="" />
                             </div>
+
                             <div className="carrosel">
-                                <img src={stateStoreDetalhe.store.fimecoData.fimistes[0].fileImgUriExternal} alt="" />
+
+
+
+                                {/**
+                               *   <img src={stateStoreDetalhe.store.fimecoData.fimistes[stateStoreDetalhe.carroselPosition].fileImgUriExternal} alt="" />
+                               */}
+                                {GenerateCarrosel()}
+
+
+                                <div className="controlCarrosel">
+                                    <div
+
+                                        onClick={e => {
+
+                                            if (stateStoreDetalhe.carroselPosition - 1 >= 0) {
+                                                setStateStoreDetalhe({
+                                                    ...stateStoreDetalhe,
+                                                    carroselPosition: stateStoreDetalhe.carroselPosition - 1
+                                                })
+                                            }
+                                        }}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" /></svg>
+
+                                    </div>
+                                    <div
+
+                                        onClick={e => {
+
+                                            if (stateStoreDetalhe.lengthCarrosel - 1 >= stateStoreDetalhe.carroselPosition + 1) {
+                                                setStateStoreDetalhe({
+                                                    ...stateStoreDetalhe,
+                                                    carroselPosition: stateStoreDetalhe.carroselPosition + 1
+                                                })
+                                            }
+                                        }}
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM406.6 278.6l-103.1 103.1c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25L306.8 288H128C110.3 288 96 273.7 96 256s14.31-32 32-32h178.8l-49.38-49.38c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l103.1 103.1C414.6 241.3 416 251.1 416 256C416 260.9 414.6 270.7 406.6 278.6z" /></svg>
+                                    </div>
+                                </div>
 
                             </div>
                         </>}
